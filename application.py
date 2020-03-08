db601369d9151c912356741a016c40adbc23b223
nimble-robotics <robotics.nimble@gmail.com>
2020-03-08 12:20:48 +0530

    added health check end point

--- application.py
+++ application.py
@@ -1,6 +1,7 @@
 from flask import Flask,request,render_template
 # from utils import upload_to_aws,bucket_name
 import os
+from healathcheck import Healathcheck
 
 
 application = app = Flask(__name__,template_folder='templates')
@@ -11,6 +12,14 @@ def home():
     return render_template('index.html')
 
 
+health = Healathcheck(app,"/healthcheck")
+health.add_check(app_status)
 
+
+def app_status():
+    """
+    added health check
+    """
+    return True,"App is up and running"
 if __name__ == '__main__':
     app.run(debug = True, host='0.0.0.0', port= 5000 )
